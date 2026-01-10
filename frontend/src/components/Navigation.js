19f90c4fbb0c71aa2b9ef978d0e57319a53a7170
Iwano123 <korkisiwan@gmail.com>
2026-01-10 23:53:29 +0100

    Feat: installed react router and added navigation.js.

---
+++ frontend/src/components/Navigation.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import './Navigation.css';
+
+function Navigation() {
+  const location = useLocation();
+
+  return (
+    <nav className="navigation">
+      <Link 
+        to="/" 
+        className={location.pathname === '/' ? 'nav-link active' : 'nav-link'}
+      >
+        Följ Användare
+      </Link>
+      <Link 
+        to="/timeline" 
+        className={location.pathname === '/timeline' ? 'nav-link active' : 'nav-link'}
+      >
+        Tidslinje
+      </Link>
+      <Link 
+        to="/wall" 
+        className={location.pathname === '/wall' ? 'nav-link active' : 'nav-link'}
+      >
+        Vägg
+      </Link>
+      <Link 
+        to="/messages" 
+        className={location.pathname === '/messages' ? 'nav-link active' : 'nav-link'}
+      >
+        Direktmeddelanden
+      </Link>
+    </nav>
+  );
+}
+
+export default Navigation;
