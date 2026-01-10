a26df2355e4246b305575210834a34ec57947b41
Iwano123 <korkisiwan@gmail.com>
2026-01-10 16:55:37 +0100

    Feat: - Add AuthController with POST /api/auth/register and POST /api/auth/login - Configure JWT authentication and Swagger Bearer token support - Register UserService and UserRepository in DI - Add PasswordHash migration to Users table

--- backend/SocialTDD.Api/obj/Debug/net9.0/SocialTDD.Api.AssemblyInfo.cs
+++ backend/SocialTDD.Api/obj/Debug/net9.0/SocialTDD.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SocialTDD.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82673dbace2c9fab1c75d451a82b2448b07ef121")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8da2c6e61c1e4eb644705ff539264f503cc233c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("SocialTDD.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SocialTDD.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
