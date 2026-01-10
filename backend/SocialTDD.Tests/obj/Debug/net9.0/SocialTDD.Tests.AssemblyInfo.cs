f988cd6ee7a8ad6cf9702b1a8fcdc2ecb1754efd
Evee98 <evelin@live.se>
2026-01-10 16:04:42 +0100

    Revert "# Om testfilen inte var committad (men den är redan det):"

--- backend/SocialTDD.Tests/obj/Debug/net9.0/SocialTDD.Tests.AssemblyInfo.cs
+++ backend/SocialTDD.Tests/obj/Debug/net9.0/SocialTDD.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SocialTDD.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04f0b1370da49e613a8fde622d7a2ddf0f59daa1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1c47f0ad297039e658e0cc7d262b0d9ebc99a29")]
 [assembly: System.Reflection.AssemblyProductAttribute("SocialTDD.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SocialTDD.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
